57baa66025e758c7568d8dd4ea2ebfd5fe115b50
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2026-08-05 01:54:01 +0300

    add ProductCategory seeder

---
+++ database/factories/ProductCategoryFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ProductCategory>
+ */
+class ProductCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $nameEn = fake()->unique()->word();
+        return [
+            'translations' => [
+                [
+                    'locale' => 'en',
+                    'name' => $nameEn,
+                ],
+                [
+                    'locale' => 'ar',
+                    'name' => fake()->word(),
+                ],
+            ],
+            'parent_id' => fake()->boolean(10) ? null : ProductCategory::factory(),
+            'is_active' => fake()->boolean(80),
+            'slug' => str($nameEn)->slug(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    // use WithoutModelEvents;
 
     /**
      * Seed the application's database.
@@ -24,5 +24,9 @@ class DatabaseSeeder extends Seeder
         Artisan::call('shield:generate --all --panel=admin --option=policies_and_permissions');
 
         $admin->assignRole('super_admin');
+
+        $this->call([
+            ProductCategorySeeder::class,
+        ]);
     }
 }

0ffa7d21085afc998765f58a05480745d853645f
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2026-08-25 18:09:02 +0300

    install api packages

---
+++ routes/api/v1.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix' => 'v1', 'as' => 'api.v1.'], function () {
+    Route::apiResource('products', ProductController::class);
+});
